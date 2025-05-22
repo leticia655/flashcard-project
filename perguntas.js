53e66310308e7da537d8416f576c1b454a56e6d7
leticia655 <tacata.leticia@escola.pr.gov.br>
2025-05-22 14:21:43 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'projeto de vida',
+    'o que sao frustraçoes?',
+    'É um sentimento negativo quando as expectativas não são cumpridas'
+)
+
+criaCartao(
+    'projeto de vida',
+    'o que significa bully em portugues?',
+    'significa valentao'
+)
+
+criaCartao(
+    'fisica1',
+    'o que é um led em robotica?',
+    'sao componentes eletrônicos que emitem luz quando energizados'
+)
